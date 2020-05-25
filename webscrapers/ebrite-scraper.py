13fb1ddfc399d4558e3e65f240468c35311232d8
Dan Reardon <dreardon1021@gmail.com>
2020-05-25 14:29:26 -0600

    Setup beautiful soup code, install lxml parser

---
+++ webscrapers/ebrite-scraper.py
@@ -0,0 +1,21 @@
+import requests # needed to access a URL
+from bs4 import BeautifulSoup
+
+# get request for a website URL, used to access the web and stores it into variable
+page_to_scrape = requests.get('https://www.eventbrite.com/d/online/free--music--events--this-month/?lang=en&page=1')
+# grabs the content from the request and stores it in variable
+page_content = page_to_scrape.content
+
+# prints 200 status code stating the page is indeed accessible uncomment below line to see status code
+# print(page_to_scrape.status_code)
+
+# Activates beautiful soup: 1st parameter is page to scrape, 2nd parameter is parser used to traverse html document
+# store this as soup for easier access
+soup = BeautifulSoup(page_content, 'lxml')
+
+#prints the html body of the provided URL, uncomment print below to see the soup body
+# print(soup.body)
+
+articles = soup.find_all("article")
+
+print(soup.prettify())

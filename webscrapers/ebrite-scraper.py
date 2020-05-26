e0f25506340562c91d4f299adb65954c2d1c7d68
Dan Reardon <dreardon1021@gmail.com>
2020-05-26 10:57:25 -0600

    Add chromedriver v.83 and grab img tags

--- webscrapers/ebrite-scraper.py
+++ webscrapers/ebrite-scraper.py
@@ -1,22 +1,22 @@
 import requests # needed to access a URL
 from bs4 import BeautifulSoup
 from csv import writer
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# get request for a website URL, used to access the web and stores it into variable
-page_to_scrape = requests.get('https://www.eventbrite.com/d/online/free--music--events--this-month/?lang=en&page=1')
-# grabs the content from the request and stores it in variable
-page_content = page_to_scrape.content
+PATH = './chromedriver'
+base_url = 'https://www.eventbrite.com/d/online/free--music--events--this-month/?lang=en&page=1'
 
-# prints 200 status code stating the page is indeed accessible uncomment below line to see status code
-# print(page_to_scrape.status_code)
 
+driver = webdriver.Chrome(PATH)
+driver.get(base_url)
+driver.implicitly_wait(100)
+
+html = driver.page_source
 # Activates beautiful soup: 1st parameter is page to scrape, 2nd parameter is parser used to traverse html document
-# store this as soup for easier access
-soup = BeautifulSoup(page_content, 'lxml')
-
-#prints the html body of the provided URL, uncomment print below to see the soup body
-# print(soup.body)
-
+soup = BeautifulSoup(html, 'html.parser')
 images = soup.find_all(class_='eds-event-card-content__image')
+
+driver.quit()
 
 print(images)

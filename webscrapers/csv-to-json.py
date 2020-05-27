f5c73065b3f1b4c0565fc4d8ea8b207760b589da
Dan Reardon <dreardon1021@gmail.com>
2020-05-27 16:58:50 -0600

    Add comments at bottom for quick description

--- webscrapers/csv-to-json.py
+++ webscrapers/csv-to-json.py
@@ -13,3 +13,9 @@ with open(csv_file_path) as csvFile:
 with open(json_file_path, 'w') as jsonFile:
   jsonFile.write(json.dumps(data, indent=2))
 
+
+# open csv file and grab the rows and create keys in the data obj as the id
+# open up the json file and write into it as jsonFile
+# dump converts the data dictionary into a json string
+# write into the json file the data which is the first argument
+# indent=2 allows the json file to be readable and not just one long line object

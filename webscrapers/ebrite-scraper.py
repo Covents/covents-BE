95fe7988f2c455d52126777016195c514cc50d7c
Dan Reardon <dreardon1021@gmail.com>
2020-05-28 14:00:09 -0600

    Add capability to scrape multiple pages

--- webscrapers/ebrite-scraper.py
+++ webscrapers/ebrite-scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from csv import writer
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementNotInteractableException
 import time
 
 PATH = './chromedriver'
@@ -20,34 +21,47 @@ with open('covents_data.csv', 'w') as csv_file:
   headers = ['id', 'event_name', 'event_image', 'event_date_time', 'event_link']
   csv_writer.writerow(headers)
 
-  html_doc.send_keys(Keys.COMMAND + 'r')
-  time.sleep(1)
-  html_doc.send_keys(Keys.PAGE_DOWN)
-  time.sleep(1)
-  html_doc.send_keys(Keys.PAGE_DOWN)
-  time.sleep(1)
-  html_doc.send_keys(Keys.PAGE_DOWN)
-  time.sleep(2)
+  id_counter = 0
 
-  html = driver.page_source
+  while True:
+    html_doc = driver.find_element_by_tag_name('html')
 
-  soup = BeautifulSoup(html, 'lxml')
+    html_doc.send_keys(Keys.COMMAND + 'r')
+    time.sleep(1)
+    html_doc.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    html_doc.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    html_doc.send_keys(Keys.PAGE_DOWN)
+    time.sleep(2)
 
-  images = soup.select('.eds-event-card-content__image')
-  event_names = soup.select('.search-event-card-square-image .eds-event-card__formatted-name--is-clamped')
-  event_date_times = soup.select('.search-event-card-square-image .eds-l-pad-bot-1')
-  event_links = soup.select('.search-event-card-square-image aside .eds-event-card-content__action-link')
 
-  counter = -1
+    html = driver.page_source
 
-  for img in images:
-    counter += 1
-    img_src = img.get('src').replace(',', '')
-    name = event_names[counter].get_text()
-    date_time = event_date_times[counter].get_text()
-    link = event_links[counter].get('href')
-    event_id = counter + 1
-    csv_writer.writerow([event_id, name, img_src, date_time, link])
+    soup = BeautifulSoup(html, 'lxml')
+
+    images = soup.select('.eds-event-card-content__image')
+    event_names = soup.select('.search-event-card-square-image .eds-event-card__formatted-name--is-clamped')
+    event_date_times = soup.select('.search-event-card-square-image .eds-l-pad-bot-1')
+    event_links = soup.select('.search-event-card-square-image aside .eds-event-card-content__action-link')
+
+    counter = -1
+
+    for img in images:
+      counter += 1
+      id_counter += 1
+      img_src = img.get('src').replace(',', '')
+      name = event_names[counter].get_text()
+      date_time = event_date_times[counter].get_text()
+      link = event_links[counter].get('href')
+      event_id = id_counter
+      csv_writer.writerow([event_id, name, img_src, date_time, link])
+
+    try:
+      next_button = driver.find_element_by_xpath("//button[@data-spec='page-next']")
+      next_button.click()
+    except ElementNotInteractableException:
+      break
 
 driver.quit()
 

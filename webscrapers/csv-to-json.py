b53a0e0aeda6052b4af143de7f353c5c05a7e167
Dan Reardon <dreardon1021@gmail.com>
2020-05-29 11:11:14 -0600

    Change csv to json to write an arary

--- webscrapers/csv-to-json.py
+++ webscrapers/csv-to-json.py
@@ -3,12 +3,18 @@ import csv, json
 csv_file_path = './covents_data.csv'
 json_file_path = './covents_json.json'
 
-data = {}
+data = []
 with open(csv_file_path) as csvFile:
   csvReader = csv.DictReader(csvFile)
   for csvRow in csvReader:
-    id = csvRow['id']
-    data[id] = csvRow
+    data_row = {
+                'id': csvRow['id'],
+                'event_name': csvRow['event_name'],
+                'image': csvRow['event_image'],
+                'event_date_time': csvRow['event_date_time'],
+                'link': csvRow['event_link']
+              }
+    data.append(data_row)
 
 with open(json_file_path, 'w') as jsonFile:
   jsonFile.write(json.dumps(data, indent=2))
